640eb7aa3dc3e757f395fcd46b9771205efc7746
Takumi Karibe <takumi.k.5610@gmail.com>
2019-05-06 21:03:33 +0900

    parse arg

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,18 @@ fn main() {
         eprintln!("引数の個数が正しくありません");
         std::process::exit(1);
     }
+
+    let arg: i32 = match args[1].parse() {
+        Ok(value) => value,
+        Err(err) => {
+            eprintln!("引数が整数ではありません\n{}", err);
+            std::process::exit(1);
+        },
+    };
+
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
-    // TODO: safety get from args
-    println!("  mov rax, {}", args[1]);
+    println!("  mov rax, {}", arg);
     println!("  ret");
 }
