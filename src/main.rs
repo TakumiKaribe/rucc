8b00d80844a65813c77025b3fb276953ae9f1244
Takumi Karibe <takumi.k.5610@gmail.com>
2019-05-06 20:36:56 +0900

    gen asm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("引数の個数が正しくありません");
+        std::process::exit(1);
+    }
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    // TODO: safety get from args
+    println!("  mov rax, {}", args[1]);
+    println!("  ret");
 }
