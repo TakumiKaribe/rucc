c89105e5e0d43fe8904ae9647a09672afd9f9af7
Takumi Karibe <takumi.k.5610@gmail.com>
2019-07-29 00:13:38 +0900

    compile add and sub

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,64 @@
+#[derive(Debug)]
+enum Operator {
+    Plus,
+    Minus,
+}
+
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
     if args.len() != 2 {
-        eprintln!("引数の個数が正しくありません");
+        eprintln!("invalid arguments");
         std::process::exit(1);
     }
-
-    let arg = args[1]
-        .parse::<i32>()
-        .unwrap_or_else(|e| panic!("引数が整数ではありません{}", e));
 
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
-    println!("  mov rax, {}", arg);
+
+    let mut arg = args.get(1).unwrap().chars().peekable();
+    let mut num = 0;
+    let mut op: Option<Operator> = None;
+    loop {
+        let peek = arg.peek();
+        if let Some(' ') = peek {
+            arg.next();
+            continue;
+        }
+
+        if peek.map_or(false, |v| v.is_digit(10)) {
+            num *= 10;
+            num += arg.next().unwrap().to_digit(10).unwrap();
+            continue;
+        }
+
+        match arg.peek() {
+            Some('+') | Some('-') => {
+                gen(&op, num);
+
+                num = 0;
+                op = arg.next().map(|c| {
+                    if c == '+' {
+                        Operator::Plus
+                    } else {
+                        Operator::Minus
+                    }
+                });
+            }
+            None => {
+                gen(&op, num);
+                break;
+            }
+            _ => panic!(""),
+        }
+    }
+
     println!("  ret");
+}
+
+fn gen(op: &Option<Operator>, num: u32) {
+    match op {
+        None => println!("  mov rax, {}", num),
+        Some(Operator::Plus) => println!("  add rax, {}", num),
+        Some(Operator::Minus) => println!("  sub rax, {}", num),
+    }
 }
