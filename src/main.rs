ef390a736b7eb5b5ff6064938a9ffebde2a26003
Takumi Karibe <takumi.k.5610@gmail.com>
2019-07-30 03:46:37 +0900

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,7 @@ fn main() {
     }
 
     let mut tokens: Vec<Token> = vec![];
-
     let mut arg = args.get(1).unwrap().chars().peekable();
-    let mut num = 0;
 
     loop {
         if let Some(' ') = arg.peek() {
@@ -30,26 +28,20 @@ fn main() {
                     Some(op.to_string()),
                 ));
                 continue;
-            }
-
+            },
             None => {
                 tokens.push(Token::new(TokenKind::EOF, None));
                 break;
-            }
-
+            },
             _ => {}
         }
 
-        loop {
-            if arg.peek().map_or(false, |v| v.is_digit(10)) {
+        let mut num = 0;
+        while arg.peek().map_or(false, |v| v.is_digit(10)) {
                 num *= 10;
                 num += arg.next().unwrap().to_digit(10).unwrap();
-            } else {
-                tokens.push(Token::new(TokenKind::Num(num), Some(num.to_string())));
-                num = 0;
-                break;
-            }
         }
+        tokens.push(Token::new(TokenKind::Num(num), Some(num.to_string())));
     }
 
     dbg!(&tokens);
